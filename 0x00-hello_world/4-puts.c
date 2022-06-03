73c269f1c46e9c53ec871e0630d77a77881661c0
mekapeu <kahtchekmekapeu@gmail.com>
2022-06-02 17:17:57 -0700

    C program that prints an entry

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
-#include <stdio>
+
+#include <stdio.h>
+
+/**
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
