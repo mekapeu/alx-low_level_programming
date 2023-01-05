2cb74456ad85cd91cb3cd399a3a460ccc3b48f36
mekapeu <kahtchekmekapeu@gmail.com>
2023-01-04 22:03:14 -0800

    You may have noticed that the basic binary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -23,33 +23,36 @@ void print_array(int *array, size_t first, size_t last)
 }
 
 /**
- * ex_binary_search - searches for a value in an array of integers
+ * binary_rec - searches for a value in an array of integers recursively
  *
  * @array: a pointer to the first element of the array to search in
- * @size: the number of elements in array
+ * @first: the minimum index of the array
+ * @last: the maximum index of the array
  * @value: the value to search for
- * @start: the first index to look inside
  *
  * Return: the index where value is located or -1 on failure or not found
  */
 
-int ex_binary_search(int *array, size_t size, int value, size_t start)
+int binary_rec(int *array, size_t first, size_t last, int value)
 {
-	size_t i = 0, first = start, last = size - 1;
+	size_t i = 0;
 
 	if (array)
 	{
-		while (first <= last)
+		if (first <= last)
 		{
 			print_array(array, first, last);
-			i = (first + last) / 2; /* Gets the middle value */
+			i = (first + last) / 2;
 
-			if (value > array[i]) /* Uses the right part of array */
-				first = i + 1;
-			else if (value < array[i]) /* Uses the left part of array */
-				last = i - 1;
-			else
+			if (array[first] == value)
+				return (first);
+
+			if (value == array[i] && array[i - 1] != value)
 				return (i);
+			if (value > array[i])
+				return (binary_rec(array, i + 1, last, value));
+			if (value <= array[i])
+				return (binary_rec(array, first, i, value));
 		}
 	}
 
@@ -57,7 +60,7 @@ int ex_binary_search(int *array, size_t size, int value, size_t start)
 }
 
 /**
- * exponential_search - searches for a value in an array of integers
+ * advanced_binary - searches for a value in an array of integers
  *
  * @array: a pointer to the first element of the array to search in
  * @size: the number of elements in array
@@ -66,22 +69,10 @@ int ex_binary_search(int *array, size_t size, int value, size_t start)
  * Return: the index where value is located or -1 on failure or not found
  */
 
-int exponential_search(int *array, size_t size, int value)
+int advanced_binary(int *array, size_t size, int value)
 {
-	size_t i = 1;
-
 	if (array)
-	{
-		while (i < size && array[i] < value)
-		{
-			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-			i *= 2;
-		}
-
-		i = i > size - 1 ? size : i + 1;
-		printf("Value found between indexes [%lu] and [%lu]\n", i / 2, i - 1);
-		return (ex_binary_search(array, i, value, i / 2));
-	}
+		return (binary_rec(array, 0, size - 1, value));
 
 	return (-1);
 }
